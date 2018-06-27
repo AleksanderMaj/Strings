c514a5e5b049fe7e0ca1be3c1c177c4d65eec6f6
Aleksander Maj <aleksander.maj@htdevelopers.com>
2018-06-27 11:33:42 +0200

    Initial Commit

---
+++ Strings/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Strings
+//
+//  Created by Aleksander Maj on 27/06/2018.
+//  Copyright © 2018 Pleez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
